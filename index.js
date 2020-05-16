d9cefff37ed768f72478164390f8ce07ae03c6a8
ashkiani <ashkiani@yahoo.com>
2020-05-16 09:21:13 -0400

    relpaced compressed.zip with Archive.zip

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
+var path = require('path');
 const unzipper = require('unzipper');
 let args = process.argv.slice(2);
 console.log(args);
 let appName = args[0];
-fs.createReadStream(`./compressed.zip`)
-  .pipe(unzipper.Extract({ path: `./${appName}` }));
+let dir = path.dirname(process.argv[1]);
+console.log(dir);
+
+fs.createReadStream(`./Archive.zip`)
+  .pipe(unzipper.Extract({ path: path.join(dir, appName) }));
